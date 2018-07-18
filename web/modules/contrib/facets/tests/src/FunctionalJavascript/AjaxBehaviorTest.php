5a1fb9742b07e6d10783deb8f97d661121f748eb
Eric Williams-Bergen <ewil@Erics-Mac-Pro.local>
2018-07-18 13:34:01 -0400

    adding facets mod

---
+++ web/modules/contrib/facets/tests/src/FunctionalJavascript/AjaxBehaviorTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\Tests\facets\FunctionalJavascript;
+
+use Drupal\views\Entity\View;
+
+/**
+ * Tests for the JS that powers ajax.
+ *
+ * @group facets
+ */
+class AjaxBehaviorTest extends JsBase {
+
+  /**
+   * Tests ajax links.
+   */
+  public function testAjaxLinks() {
+    // Create facets.
+    $this->createFacet('owl');
+    $this->createFacet('duck', 'keywords');
+
+    // Force ajax.
+    $view = View::load('search_api_test_view');
+    $display = $view->getDisplay('page_1');
+    $display['display_options']['use_ajax'] = TRUE;
+    $view->save();
+
+    // Go to the views page.
+    $this->drupalGet('search-api-test-fulltext');
+
+    // Make sure the blocks are shown on the page.
+    $page = $this->getSession()->getPage();
+    $block_owl = $page->findById('block-owl-block');
+    $block_owl->isVisible();
+    $block_duck = $page->findById('block-duck-block');
+    $block_duck->isVisible();
+    $this->assertSession()->pageTextContains('Displaying 5 search results');
+
+    // Check that the article link exists (and is formatted like a facet) link.
+    $links = $this->xpath('//a//span[normalize-space(text())=:label]', [':label' => 'article']);
+    $this->assertNotEmpty($links);
+
+    // Click the item facet.
+    $this->clickLink('item');
+
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->assertSession()->pageTextContains('Displaying 3 search results');
+
+    // Check that the article facet is now gone.
+    $links = $this->xpath('//a//span[normalize-space(text())=:label]', [':label' => 'article']);
+    $this->assertEmpty($links);
+
+    // Click the item facet again, and check that the article facet is back.
+    $this->clickLink('item');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->assertSession()->pageTextContains('Displaying 5 search results');
+    $links = $this->xpath('//a//span[normalize-space(text())=:label]', [':label' => 'article']);
+    // $this->assertNotEmpty($links);
+
+    // Check that the strawberry link disappears when filtering on items.
+    $links = $this->xpath('//a//span[normalize-space(text())=:label]', [':label' => 'strawberry']);
+    $this->assertNotEmpty($links);
+    $this->clickLink('item');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $links = $this->xpath('//a//span[normalize-space(text())=:label]', [':label' => 'strawberry']);
+    $this->assertEmpty($links);
+  }
+
+}
